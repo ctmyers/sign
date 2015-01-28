8b0d00e16a89c279a6d3813aeadee2a405ffa5bc
waharris <will.alexander.harris@gmail.com>
2015-01-28 02:03:46 -0500

    removed tests from network interface

--- protocol/interface.py
+++ protocol/interface.py
@@ -17,15 +17,7 @@ class NetworkInterface(Interface):
         super(NetworkInterface, self).__init__(ip, port)
 
     def send(self, command):
-        print command.data
-        for c in command.data:
-            print ord(c), hex(ord(c)), c, 'a'
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         sock.connect((self.ip, self.port))
         sock.send(command.data)
         sock.close()
-
-
-from text import Text
-sign = NetworkInterface()
-sign.send(Text('lmaooo').command)
