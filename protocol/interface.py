dfed30d8da920a2cd979fc2fcc69c565ef9a2c0e
waharris <will.alexander.harris@gmail.com>
2015-02-10 16:43:48 -0500

    hopefully fixed socket sending failing if done too fast (<5s) on linux

--- protocol/interface.py
+++ protocol/interface.py
@@ -16,9 +16,24 @@ import socket
 class NetworkInterface(Interface):
     def __init__(self, ip=config.ip, port=config.port):
         super(NetworkInterface, self).__init__(ip, port)
+        self.sock = None
+
+    def connect(self):
+        if self.sock:
+            self.sock.close()
+            self.sock = None
+
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.connect((self.ip, self.port))
 
     def send(self, command):
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sock.connect((self.ip, self.port))
-        sock.send(command.data)
-        sock.close()
+        if self.sock is None:
+            self.connect()
+
+        try:
+            self.sock.sendall(command.data)
+        except socket.error:
+            self.connect()
+
+    def __del__(self):
+        self.sock.close()
