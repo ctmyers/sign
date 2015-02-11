a6f548e90c84789b32b2cca1ae51c569c2b486d1
waharris <will.alexander.harris@gmail.com>
2015-02-10 21:48:59 -0500

    weather now just needs formatting

--- plugins/weather.py
+++ plugins/weather.py
@@ -2,6 +2,7 @@ from yapsy.IPlugin import IPlugin
 from manager.message import Message
 
 from protocol.string import String
+import protocol.control as control
 
 from collections import OrderedDict
 import json
@@ -15,12 +16,11 @@ class Weather(IPlugin, Message):
         IPlugin.__init__(self)
         Message.__init__(self)
 
-        self.text = '|%s| [%s] l%sl'
+        self.text = control.SPEED_1 + '%s' + control.NEW_PAGE + '%s'
         self.schedule = schedule.every(1).minutes
         self.location = '20740'
-
-        self.get_weather()
-        self.get_precipitation()
+        self.lat = '39.003'
+        self.lon = '-76.935'
 
     def get_weather(self):
         url = 'https://query.yahooapis.com/v1/public/yql?q=' \
@@ -31,39 +31,36 @@ class Weather(IPlugin, Message):
         data = data['query']['results']['channel']
         today, tomorrow = {}, {}
 
-        temp_units = data['units']['temperature'].encode('ascii', 'ignore')
-        print temp_units
+        units = data['units']['temperature'].encode('ascii', 'ignore')
 
         temp = data['item']['condition']['temp'].encode('ascii', 'ignore')
         humidity = data['atmosphere']['humidity'].encode('ascii', 'ignore')
-        print temp, humidity
 
         today['high'] = data['item']['forecast'][0]['high'].encode('ascii', 'ignore')
         today['low'] = data['item']['forecast'][0]['low'].encode('ascii', 'ignore')
         today['condition'] = data['item']['forecast'][0]['text'].encode('ascii', 'ignore')
-        print today
 
         tomorrow['high'] = data['item']['forecast'][1]['high'].encode('ascii', 'ignore')
         tomorrow['low'] = data['item']['forecast'][1]['low'].encode('ascii', 'ignore')
         tomorrow['condition'] = data['item']['forecast'][1]['text'].encode('ascii', 'ignore')
-        print tomorrow
+
+        return {'temp': temp, 'humidity': humidity, 'units': units,
+                'today': today, 'tomorrow': tomorrow}
 
     def get_precipitation(self):
-        lat = '39.003'
-        lon = '-76.935'
         url = 'http://isitgoingtorain.com/data/nearest.php?' \
-              'callback=iigtrData&latitude=%s&longitude=%s' % (lat, lon)
+              'callback=iigtrData&latitude=%s&longitude=%s' % (self.lat, self.lon)
 
         data = json.loads(urllib.urlopen(url).read()[10:-1])
 
         days = {time.strptime(k[:-6], '%a, %d %b %Y %H:%M:%S'): data['Pp'][k] for k in data['Pp']}
         s = sorted(days)
 
-        print 'evening precip: %s' % days[s[0]]
-        print 'morning precip: %s' % days[s[1]]
+        return {'morning': days[s[0]], 'evening': days[s[1]]}
 
     def get_commands(self):
-        return [String('11', self.labels_string[0]),
-                String('23', self.labels_string[1]),
-                String('35', self.labels_string[2])]
+        weather = self.get_weather()
+        precip = self.get_precipitation()
 
+        return [String('%s' % weather['temp'], self.labels_string[0]),
+                String('rain: %s' % precip['morning'], self.labels_string[1])]
