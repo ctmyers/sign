c4bcf49fe42ba9f78b6a8b729079e2a156065ffa
waharris <will.alexander.harris@gmail.com>
2015-02-17 19:49:51 -0500

    refresh sign time when script is run

--- sign.py
+++ sign.py
@@ -2,7 +2,14 @@
 
 from protocol.interface import NetworkInterface
 from manager.manager import Manager
+from protocol.special import TimeOfDay, DayOfWeek
 
 sign = NetworkInterface()
+
+# set current time
+sign.send(TimeOfDay())
+sign.send(DayOfWeek())
+
+# run plugins
 manager = Manager(sign)
 manager.run()
