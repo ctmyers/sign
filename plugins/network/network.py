1b8bdfadc4810d82f1d6f8b63f49ffd36e818b0a
waharris <will.alexander.harris@gmail.com>
2015-03-03 00:48:15 -0500

    adapt (and fix) network plugin

--- plugins/network/network.py
+++ plugins/network/network.py
@@ -8,7 +8,21 @@ import protocol.control as control
 
 import schedule
 import requests
+import re
 from bs4 import BeautifulSoup
+
+
+def sizeof_fmt(num, suffix='B'):
+    for unit in ['', 'K', 'M', 'G', 'T', 'P', 'E', 'Z']:
+        if abs(num) < 1024.0:
+            if abs(num) < 100.0:
+                if abs(num) < 10.0:
+                    return "%1.2f%s%s" % (num, unit, suffix)
+                return "%2.1f%s%s" % (num, unit, suffix)
+            return "%3.0f%s%s" % (num, unit, suffix)
+        num /= 1024.0
+    return "%.1f%s%s" % (num, 'Y', suffix)
+
 
 class Network(IPlugin, Message):
     def __init__(self):
@@ -16,75 +30,62 @@ class Network(IPlugin, Message):
         Message.__init__(self)
 
         self.text = control.SPEED_1 + '%s' + control.NEW_LINE + '%s'
-        self.schedule = schedule.every(1).hours
+        self.schedule = schedule.every(1).minutes
 
-        lines = [line.strip() for line in open('filename')]
-        config = {}
-        for line in lines:
-            line = line.rstrip.spilt("=")
-            config[line[0]] = line[1]
+        c = {}
+        with open('plugins/network/network.config', 'r') as conf:
+            for line in conf.readlines():
+                s = line.split('=')
+                c[s[0].strip()] = s[1].strip()
 
-        self.ip = config["ip"]
-        self.user = config["user"]
-        self.password = config["password"]
+        self.ip = c['ip']
+        self.user = c['user']
+        self.password = c['password']
 
     def get_bandwidth(self):
         c = Network._convert
-        _get_control()
-        r = requests.get('http://' + self.ip + "/RST_stattbl.htm", auth=(self.user,self.password))
+        self.get_control()
+
+        r = requests.get('http://' + self.ip + "/RST_stattbl.htm", auth=(self.user, self.password))
         soup = BeautifulSoup(r.text)
-        data = []
         table = soup.find('table', border='1', cellpadding='0')
-        data = _get_data_from_table(table)
+        data = Network._get_data_from_table(table)
+
         if len(data) is not 0:
-            upstream = _bytes_to_mb(float(c(data[1][5])))
-            downstream = _bytes_to_mb(float(float(c(data[1][6]))))
-            print "upstream:   %s" % upstream
-            print "downstream: %s" % downstream
+            upstream = sizeof_fmt(float(c(data[1][5])))
+            downstream = sizeof_fmt(float(float(c(data[1][6]))))
             return {'up': upstream, 'down': downstream}
+
         return None
 
     def get_total(self):
         c = Network._convert
-        _get_control()
-        r = requests.get('http://' + '192.168.1.1' + "/traffic_meter.htm", auth=('admin','password'))
+        self.get_control()
+
+        r = requests.get('http://' + self.ip + "/traffic_meter.htm", auth=(self.user, self.password))
         soup = BeautifulSoup(r.text)
         table = soup.find('table', border='1', cellpadding='0')
-        data = _get_data_from_table(table)
-        if len(data) is not 0:
-            total_uploaded_today = _mb_to_gb(float(c(data[2][2])))
-            total_downloaded_today = _mb_to_gb(float(c(data[2][3])))
-            total_uploaded_month = _mb_to_gb(float(c(data[5][2]).split("/")[0].strip()))
-            total_downloaded_month = _mb_to_gb(float(c(data[5][3]).split("/")[0].strip()))
-            print "total_uploaded_today:   %s" % total_downloaded_today
-            print "total_downloaded_today: %s" % total_uploaded_today
-            print "total_uploaded_month:   %s" % total_uploaded_month
-            print "total_downloaded_month: %s" % total_downloaded_month
-            return {'uptd': total_uploaded_today, 'dwtd': total_downloaded_today, 'upmnth': total_uploaded_month, 'dwmnth': total_downloaded_month}
+        data = Network._get_data_from_table(table)
+
+        if len(data) != 0:
+            total_uploaded_today = sizeof_fmt(float(c(data[2][2])) * 1000000)
+            total_downloaded_today = sizeof_fmt(float(c(data[2][3])) * 1000000)
+            total_uploaded_month = sizeof_fmt(float(c(data[5][2]).split("/")[0].strip()) * 1000000)
+            total_downloaded_month = sizeof_fmt(float(c(data[5][3]).split("/")[0].strip()) * 1000000)
+            return {'up_today': total_uploaded_today, 'down_today': total_downloaded_today,
+                    'up_month': total_uploaded_month, 'down_month': total_downloaded_month}
+
         return None
 
-    def _mb_to_gb(self,data):
-        units = "MB"
-        if data > 1024:
-            units = "GB"
-            data = data / 1024
-        return "%s %s" % data, units
-
-    def _bytes_to_mb(self, data):
-        units = "KB/s"
-        data = data / 1024
-        if data > 1024:
-            data = data / 1024
-            units = "MB/s"
-        return "%s %s" % data, units
-
-    def _get_control(self):
-        r= requests.get('http://' + '192.168.1.1' + '/start.htm', auth=('admin','password'))
+    def get_control(self):
+        r = requests.get('http://' + self.ip + '/start.htm', auth=(self.user, self.password))
         table = BeautifulSoup(r.text).find('table', cellpadding='0', cellspacing='0')
         if table is None:
-            r= requests.get('http://' + '192.168.1.1' + '/multi_login.cgi?id=1766147816', auth=('admin','password'), params={'act':'yes'})
+            r = requests.get('http://' + self.ip + '/multi_login.cgi?id=1766147816',
+                             auth=(self.user, self.password), params={'act': 'yes'})
 
-    def _get_data_from_table(self, table):
+    @staticmethod
+    def _get_data_from_table(table):
         pattern = re.compile('(\d\s\W)*')
         data = []
         if table is None:
@@ -100,8 +101,8 @@ class Network(IPlugin, Message):
                     if match is not None and text is not "":
                         columns.append(text)
 
-                if len(columns) is not 0:
-                    data.append(columns) # Get rid of empty values
+                if len(columns) != 0:
+                    data.append(columns)  # Get rid of empty values
         return data
 
     @staticmethod
@@ -111,15 +112,14 @@ class Network(IPlugin, Message):
         else:
             return str(value)
 
-
     def get_commands(self):
         w = self.get_total()
-        if w != None:
-            return (String('^ today:%s %s v today:%s %s ^ month:%s %s v month: %s' % (w['uptd'],
-                                                control.NEW_LINE,
-                                                w['dwtd'],
-                                                control.NEW_LINE,
-                                                w['upmnth']),
-                                                control.NEW_LINE,
-                                                w['dwmnth']))
-        return None
+
+        if w:
+            return (String('Today %s/%s' %
+                           (w['up_today'], w['down_today']), self.labels_string[0]),
+                    String('This Month %s/%s' %
+                           (w['up_month'], w['down_month']), self.labels_string[1]))
+
+        return (String('network failed', self.labels_string[0]),
+                String('', self.labels_string[1]))
